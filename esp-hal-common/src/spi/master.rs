c8886523a4a1b810d94ce92db165a90f92d7ef4c
Fan Jiang <i@fanjiang.me>
2023-11-14 16:37:27 -0500

    stupid change

--- esp-hal-common/src/spi/master.rs
+++ esp-hal-common/src/spi/master.rs
@@ -2628,6 +2628,7 @@ pub trait Instance {
     /// you must ensure that the whole messages was written correctly, use
     /// [`Self::flush`].
     // FIXME: See below.
+    #[inline]
     fn write_bytes(&mut self, words: &[u8]) -> Result<(), Error> {
         let num_chunks = words.len() / FIFO_SIZE;
 
@@ -2708,7 +2709,7 @@ pub trait Instance {
     // FIXME: Using something like `core::slice::from_raw_parts` and
     // `copy_from_slice` on the receive registers works only for the esp32 and
     // esp32c3 varaints. The reason for this is unknown.
-    #[inline]
+    #[inline(never)]
     fn read_bytes_from_fifo(&mut self, words: &mut [u8]) -> Result<(), Error> {
         let reg_block = self.register_block();
         for chunk in words.chunks_mut(FIFO_SIZE) {
