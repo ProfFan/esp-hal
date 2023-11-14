f68906c75984eb585f92ee5e43e0607d9c153a7e
Fan Jiang <ProfFan@users.noreply.github.com>
2023-11-14 15:29:55 -0500

    use copy

--- esp-hal-common/src/spi/master.rs
+++ esp-hal-common/src/spi/master.rs
@@ -2721,7 +2721,7 @@ pub trait Instance {
                 let bytes = reg_val.to_le_bytes();
 
                 let len = usize::min(chunk.len(), index + 4) - index;
-                chunk[index..(index + len)].clone_from_slice(&bytes[0..len]);
+                chunk[index..(index + len)].copy_from_slice(&bytes[0..len]);
 
                 unsafe {
                     fifo_ptr = fifo_ptr.offset(1);
